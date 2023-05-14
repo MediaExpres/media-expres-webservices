eff3cad1e9f4e8689e8c77ca2aae6d5acbb627e8
Mihai Pavelescu <media.expres@gmail.com>
2023-05-14 16:16:51 +0300

    maybe now goes right

--- pages/index.js
+++ pages/index.js
@@ -100,7 +100,7 @@ export default function Home() {
               Deploy <span>-&gt;</span>
             </h2>
             <p>
-              From domain reservation, webhosting to deploying we work directly with RoTLD and Romarg but not only.
+              From domain reservation, webhosting to deploying we work directly with RoTLD and Romarg but not only. Lignum-mobilier.ro and brutariaforni.ro are the living examples of our commitement. With under 500 Euro per year.
             </p>
           </a>
 
