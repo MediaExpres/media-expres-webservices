8d610d3e1cdf66dd992c593eaeb47686b3b51393
Mihai Pavelescu <media.expres@gmail.com>
2023-05-17 16:15:18 +0300

    editcontacts

--- pages/docs.js
+++ pages/docs.js
@@ -32,7 +32,7 @@ export default function Home() {
                 height={24}
                 priority
               />
-              <p> &copy; Media Expres SRL <script>document.write(new Date().getFullYear());</script> | <a href="mailto:sales@mediaexpres.net">sales@mediaexpres.net</a> | +40730656959</p>
+              <p> &copy; Media Expres SRL <script>document.write(new Date().getFullYear());</script>  <a href="mailto:sales@mediaexpres.net">sales@mediaexpres.net</a> tel: +40730656959</p>
   
             </a>
           </div>
