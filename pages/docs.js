d49d9de1c2f5710ff03b52ef4554e287cb628ef3
Mihai Pavelescu <media.expres@gmail.com>
2023-05-17 15:24:15 +0300

    fara br-uri

--- pages/docs.js
+++ pages/docs.js
@@ -60,19 +60,19 @@ export default function Home() {
             <p>
               We specialize in full-stack development, server administration, and providing the most affordable solutions for taking your web presence from zero to online. Our team is dedicated to delivering high-quality websites tailored to your needs.
             </p>
-            <br>
+            
             <p>
               Whether you require a simple personal website or a complex e-commerce platform, we have the expertise to bring your vision to life. Our full-stack developers ensure that both the front-end and back-end of your website are robust, user-friendly, and visually appealing.
             </p>
-            <br>
+            
             <p>
               Our server administrators guarantee smooth operations by managing server configurations, security, and performance optimization. We handle all the technical aspects, allowing you to focus on your core business.
             </p>
-            <br>
+            
             <p>
               MediaExpres Web Services prides itself on providing cost-effective solutions. We understand the challenges faced by startups and small businesses, and we offer affordable packages to get your website up and running without breaking the bank.
             </p>
-            <br>
+            
             <p>
               Contact us today to discuss your web development needs and let us help you establish a strong online presence.
             </p>
