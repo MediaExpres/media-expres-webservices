f54562d8f888cbccc52d2da7995db2ed8130126e
Mihai Pavelescu <media.expres@gmail.com>
2023-05-17 15:21:00 +0300

    br

--- pages/docs.js
+++ pages/docs.js
@@ -60,7 +60,7 @@ export default function Home() {
             <p>
               We specialize in full-stack development, server administration, and providing the most affordable solutions for taking your web presence from zero to online. Our team is dedicated to delivering high-quality websites tailored to your needs.
             </p>
-<br></br>
+<br>
             <p>
               Whether you require a simple personal website or a complex e-commerce platform, we have the expertise to bring your vision to life. Our full-stack developers ensure that both the front-end and back-end of your website are robust, user-friendly, and visually appealing.
             </p>
