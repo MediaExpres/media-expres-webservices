7e544c1476710977b1ede986de2e0e473889a3f8
Mihai Pavelescu <media.expres@gmail.com>
2023-05-26 00:10:45 +0300

    year with react usestate use effect

--- pages/docs.js
+++ pages/docs.js
@@ -1,13 +1,21 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { useEffect, useState } from 'react'
 import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [currentYear, setCurrentYear] = useState('');
+
+  useEffect(() => {
+    const year = new Date().getFullYear();
+    setCurrentYear(year.toString());
+  }, []);
+
   return (
-<>
+    <>
       <Head>
         <title>MEDIA EXPRES WEBSERVICES</title>
         <meta name="description" content="The presentation page of the web developer services offered by Media Expres" />
@@ -16,7 +24,7 @@ export default function Home() {
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.description}>
-         <div>
+          <div>
             <a
               href="https://www.mediaexpres.ro"
               target="_blank"
@@ -33,11 +41,12 @@ export default function Home() {
               />
             </a>
           </div>
-       </div>
-    
-    <div> <h1>Welcome to </h1></div>
+        </div>
+        <div>
+          <h1>Welcome to </h1>
+        </div>
         <div className={styles.center}>
-               <Image
+          <Image
             className={styles.logo}
             src="/banner_mediaexpres_webservices.svg"
             alt="Media Expres Webservices Logo"
@@ -46,38 +55,29 @@ export default function Home() {
             priority
           />
         </div>
-    <div>
+        <div>
           <a
             href="https://www.mediaexpres.ro/contact.aspx"
             className={styles.docs}
             target="_blank"
             rel="noopener noreferrer"
           >
-           <p>
+            <p>
               We specialize in full-stack development, server administration, and providing the most affordable solutions from zero to online. Our team is dedicated to delivering fast high-quality websites tailored to any needs.
-              Whether you require a simple personal website or a complex e-commerce platform, we have the expertise to bring your vision to life. 
+              Whether you require a simple personal website or a complex e-commerce platform, we have the expertise to bring your vision to life.
               Our server administrators guarantee smooth operations by managing server configurations, security, and performance optimization. We handle all the technical aspects, allowing you to focus on your core business.
-           </p>
+            </p>
           </a>
-       </div>
+        </div>
 
-       <footer>
-        <p> &copy; Media Expres SRL&nbsp; 
-        <p>The current year is: <span id="year"></span></p>
-
-<script>
-  var yearElement = document.getElementById("year");
-  yearElement.textContent = `${new Date().getFullYear()}`;
-</script>
-         <a href="mailto:sales@mediaexpres.net">sales@mediaexpres.net</a>
-         &nbsp;tel: +40730656959
+        <footer>
+          <p> &copy; Media Expres SRL&nbsp;
+            The current year is: {currentYear}
+            <a href="mailto:sales@mediaexpres.net">sales@mediaexpres.net</a>
+            &nbsp;tel: +40730656959
           </p>
-    
         </footer>
-    
-      
-       </main>
-  </>  
-    
+      </main>
+    </>
   )
 }
