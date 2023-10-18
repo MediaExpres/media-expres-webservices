e4d1fcdc13596a5aa88a2a74acf73824719a3b19
Mihai Pavelescu <media.expres@gmail.com>
2023-10-18 14:31:43 +0300

    new try in _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,20 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head> 
+      <script async src='https://www.googletagmanager.com/gtag/js?id=G-VPHL96LY7Q'></script>
+<script
+    dangerouslySetInnerHTML={{
+        __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments)}
+            gtag('js', new Date());
+            
+            gtag('config', 'G-VPHL96LY7Q');
+              `
+      }}>
+</script>
+      </Head>
       <body>
         <Main />
         <NextScript />
