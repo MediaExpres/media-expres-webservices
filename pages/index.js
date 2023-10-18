6545b15640a2f6f9d873810a625aaa564cb9a6d8
Mihai Pavelescu <media.expres@gmail.com>
2023-10-18 12:34:38 +0300

    google tag added

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,17 @@ export default function Home() {
         <meta name="description" content="The presentation page of the web developer services offered by Media Expres" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-48173861-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-48173861-1');
+</script>
+
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.description}>
