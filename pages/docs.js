a4f3c82d96d7760f13f757bcb856de1a33dd5233
Mihai Pavelescu <media.expres@gmail.com>
2024-03-27 14:04:24 +0200

    Update docs.js
    commenting out due to x-Frame Options, I don't know if they will allow me

--- pages/docs.js
+++ pages/docs.js
@@ -128,11 +128,11 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             
-            <strong>MEGOGO Copy&Subs</strong>
-            <a width="100%" height="100%" href="https://www.megogo.net"></a>
+  //           <strong>MEGOGO Copy&Subs</strong>
+  //           <a width="100%" height="100%" href="https://www.megogo.net"></a>
 
   //          <iframe width="100%" height="100%" src="https://megogo.net/ro"></iframe>
-          </a>
+  //         </a>
 
         </div>
       
